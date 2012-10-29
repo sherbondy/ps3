24e1c43c1fb6d706fbfb30eccd4a4b0e235c97a0
Ethan Sherbondy <sherbondye@gmail.com>
2012-10-29 03:52:11 -0400

    naive gibbs sampling done. Now to improve with relative entropy.

--- code/gibbs.clj
+++ code/gibbs.clj
@@ -17,15 +17,17 @@
 (def bases "ATGC")
 
 (defn remove-nth [v n]
-  (keep-indexed
-   (fn [i e] (if (not= i n) e))
-   v))
+  (vec (keep-indexed
+        (fn [i e] (if (not= i n) e))
+        v)))
+
+;; (remove-nth [0 1 2 3 4 5] 2)
 
 (defn subseqs [seqs len]
-  (for [seq seqs]
-    (let [start (rand-int (- (count seq) len))
-          end   (+ start len)]
-      (subs seq start end))))
+  (vec (for [seq seqs]
+         (let [start (rand-int (- (count seq) len))
+               end   (+ start len)]
+           (subs seq start end)))))
 
 ;; (str (get-in seqs [0 0]))
 
@@ -41,9 +43,8 @@
            (fn [[k v]] {k (/ v fcount)})
            (frequencies v)))))
 
-((freq-dist "ATGCC") (nth bases 1))
-
-(freq-dist (map #(nth % 0) (subseqs seqs 8)))
+;; ((freq-dist "ATGCC") (nth bases 1))
+;; (freq-dist (map #(nth % 0) (subseqs seqs 8)))
 
 (defn make-profile [subseqs size bases]
   ;; first index = letter
@@ -57,13 +58,97 @@
       )))
     arr))
 
-(defn gibbs [seqs len]
-  (let [ss (subseqs seqs len)
-        sample-idx (rand-int (count seqs))
-        new-ss (remove-nth ss sample-idx)
-        profile (make-profile new-ss len bases)]
-    (pprint new-ss)
-    (pprint profile)
-    [sample-idx new-ss]))
+(defn l-mer [s i len]
+  (subs s i (+ i len)))
 
-(gibbs seqs 8)
+(defn l-mers
+  "Returns all subsequences of s with length len"
+  [s len]
+  (for [i (range (- (count s) len))]
+    (l-mer s i len)))
+    
+(defn bi [base]
+  (.indexOf bases (str base)))
+
+;; (bi \T) => 1
+
+(defn lmer-prob [lmer profile]
+  (reduce * (map-indexed (fn [i base]
+                           (get-in profile [(bi base) i]))
+                         lmer)))
+
+;; from http://rosettacode.org/wiki/Probabilistic_choice#Clojure
+(defn to-cdf [pdf]
+  (reduce
+    (fn [acc n] (conj acc (+ (or (last acc) 0) n)))
+    []
+    pdf))
+
+; (to-cdf [1 2.0 1 1])
+
+(defn rank-lmers [lmers profile]
+  (let [probs      (map #(lmer-prob % profile) lmers)
+        nonzeros   (filter #(not= % 0.0) probs)
+        max-prob   (apply max probs)
+        min-prob   (if (not (zero? (count nonzeros)))
+                     (apply min nonzeros))
+        prob-dist  (if min-prob
+                     (map #(/ % min-prob) probs)
+                     (repeat (count lmers) 1))
+        prob-cdf   (to-cdf prob-dist)
+        rand-f     (rand (last prob-cdf))
+        new-start  (count (filter #(< % rand-f) prob-cdf))]
+    [new-start max-prob]))
+
+;; this should yield a uniform dist over start positions
+(defn test-ranker []
+  (rank-lmers ["AAA" "ATG" "CAG"]
+              (to-array-2d [[0.5 0.5 0.5]
+                            [0.0 0.5 0.0]
+                            [0.0 0.0 0.5]
+                            [0.5 0.0 0.0]])))
+
+;; it works well
+(frequencies
+ (for [i (range 10000)]
+  (test-ranker)))
+
+(defn gibbs-iter [seqs len ss]
+  (let [removed-idx (rand-int (count seqs))
+        removed-seq (nth seqs removed-idx)
+        new-ss      (remove-nth ss removed-idx)
+        profile     (make-profile new-ss len bases)
+        [new-start max-prob] (rank-lmers (l-mers removed-seq len)
+                                         profile)
+        added-ss    (l-mer removed-seq new-start len)]
+    ;;(pprint new-ss)
+    ;;(pprint profile)
+    (pprint max-prob)
+
+    ;; must return new ss in order!
+    ;; ss must be a vector for this to work
+    [max-prob
+     added-ss
+     (reduce conj (conj (subvec ss 0 removed-idx)
+                        added-ss)
+             (subvec ss (inc removed-idx)))]))
+
+;; Idea: stop iterating once we've gone cutoff rounds
+;; without seeing an increase in max-prob
+
+(defn gibbs [seqs len cutoff]
+  (println "Beginning gibbs")
+  ;; rounds = the number of rounds without improvement
+  (loop [ss (subseqs seqs len) best 0.0 rounds 0]
+    (let [[max-prob added-ss new-ss] (gibbs-iter seqs len ss)
+          new-best                   (max max-prob best)
+          rounds                     (if (> new-best best)
+                                       0
+                                       (inc rounds))]
+      
+      (if (< rounds cutoff)
+        (recur new-ss new-best rounds)
+        [new-best added-ss new-ss])
+      )))
+
+(gibbs seqs 8 50)
